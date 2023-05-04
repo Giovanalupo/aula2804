c0361beaf084410581bbc6385ea2f4a211ef5d2a
lilgab4m <130669520+lilgab4m@users.noreply.github.com>
2023-05-04 10:13:07 -0300

    Update CalculadoraSimples.java

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -22,7 +22,27 @@ public class CalculadoraSimples{
 
             if(op == 1){
                 System.out.println("\n\t\t\t -- Soma --\n"); 
-            } else if(op == 2){
+            } else if(op == 2){ // Cria um objeto Scanner para ler a entrada do usuário
+                Scanner scanner = new Scanner(System.in);
+                
+                // Solicita o primeiro número
+                System.out.print("Digite o primeiro número: ");
+                int numero1 = scanner.nextInt();
+                
+                // Solicita o segundo número
+                System.out.print("Digite o segundo número: ");
+                int numero2 = scanner.nextInt();
+                
+                // Realiza a soma
+                int resultado = numero1 + numero2;
+                
+                // Exibe o resultado
+                System.out.println("A soma dos números é: " + resultado);
+                
+                // Fecha o objeto Scanner
+                scanner.close();
+            }
+        }
                 System.out.println("\n\t\t\t -- Subtração -- \n");
             } else if(op == 3){
                 System.out.println("Forte abraço!");
