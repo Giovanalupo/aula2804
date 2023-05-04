0d5e25b21e1946df88c3ed2534d2960d31e73f4c
felipinaseosd <130668980+felipinaseosd@users.noreply.github.com>
2023-05-04 10:08:08 -0300

    Update CalculadoraSimples.java

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -32,35 +32,6 @@ public class CalculadoraSimples{
             
         }while(op!=3);
 
+
     }
-    public class Soma {
-        public static void main(String[] args) {
-            // Cria um objeto Scanner para ler a entrada do usuário
-            Scanner scanner = new Scanner(System.in);
-            
-            // Solicita o primeiro número
-            System.out.print("Digite o primeiro número: ");
-            int numero1 = scanner.nextInt();
-            
-            // Solicita o segundo número
-            System.out.print("Digite o segundo número: ");
-            int numero2 = scanner.nextInt();
-            
-            // Realiza a soma
-            int resultado = numero1 + numero2;
-            
-            // Exibe o resultado
-            System.out.println("A soma dos números é: " + resultado);
-            
-            // Fecha o objeto Scanner
-            scanner.close();
-        }
-        public class SubtractionExample {
-   public static void main(String[] args) {
-      int num1 = 10;
-      int num2 = 5;
-      int result = num1 - num2;
-      System.out.println("Subtraction result is: " + result);
-   }
-}
 }
